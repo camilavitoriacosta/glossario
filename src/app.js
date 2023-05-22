816ad8cebaf3419b59cc9f6225af199ea415f726
Camila Vitoria <camilavitoria.c.costa@gmail.com>
2023-05-22 16:01:24 -0400

    implementação da listagem com dados dinamicos

--- src/app.js
+++ src/app.js
@@ -1,11 +1,3 @@
-import { inject } from 'aurelia-framework';
-import { TermoApi } from './termo-api';
-
-@inject(TermoApi)
 export class App {
 
-  constructor(termoApi){
-    this.termoApi = termoApi;
-    this.termos = this.termoApi.obterTermos();
-  }
 }
--- src/components/lista-termos/lista-termos.js
+++ src/components/lista-termos/lista-termos.js
@@ -1,19 +1,22 @@
-import { inject, json } from 'aurelia-framework';
+import { inject } from 'aurelia-framework';
 import { TermoApi } from '../../termo-api';
 
 @inject(TermoApi)
 export class ListaTermos {
 
-  constructor(termoApi){
+  constructor(termoApi) {
     this.termoApi = termoApi;
+    this.termos = [];
     this.obterTermos();
   }
-  
+
   attached() {
     this.obterTermos();
   }
 
-  obterTermos(){
-    this.termoApi.obterTermos().then(resposta => resposta.json()).then(resposta => this.termos = resposta);
+  obterTermos() {
+    this.termoApi.obterTermos().then(resposta => resposta.json()).then(resposta => {
+      this.termos = resposta;
+    });
   }
 }
