1cd31496e714cfd41ac69012c9bd186789d0ee52
Camila Vitoria <camilavitoria.c.costa@gmail.com>
2023-06-05 12:21:02 -0400

    ajustando filtragem

--- src/components/filtro-alfabetico/filtro-alfabetico.js
+++ src/components/filtro-alfabetico/filtro-alfabetico.js
@@ -1,24 +1,29 @@
-import { bindable } from 'aurelia-framework';
+import { bindable, inject } from 'aurelia-framework';
+import { TermoApi } from '../../termo-api';
 
+
+@inject(TermoApi)
 export class FiltroAlfabetico {
   @bindable controladorListaTermos;
   letraSelecionada = '';
   termos = [];
   
-  constructor() {
+  constructor(termoApi) {
     this.letrasAlfabeto = [
       "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
       "S", "T", "U", "V", "W", "X", "Y", "Z"
     ];
     this.letraSelecionada = this.letrasAlfabeto[0];
+    this.termoApi = termoApi;
   }
 
   attached(){
-    this.termos = this.controladorListaTermos.termos;
+    this.termoApi.obterTermos().then(resposta => resposta.json()).then(resposta => {
+      this.termos = resposta;
+    });
   }
 
   filtrar() {
     this.controladorListaTermos.termos = this.termos.filter(termo => termo.termo.charAt(0).toUpperCase() == this.letraSelecionada);
-    console.log(this.termos);
   }
 }
