ddaef401c0d8e0d8eb9cfc1a9e2c93b408d855ad
Camila Vitoria <camilavitoria.c.costa@gmail.com>
2023-06-19 13:34:34 -0400

    criando validacao de campo e contador

--- src/components/dialogo-de-criacao-de-termo/dialogo-de-criacao-de-termo.js
+++ src/components/dialogo-de-criacao-de-termo/dialogo-de-criacao-de-termo.js
@@ -1,5 +1,6 @@
-import { inject } from 'aurelia-framework';
+import { inject, computedFrom } from 'aurelia-framework';
 import { TermoApi } from "../../termo-api";
+const _ = require("lodash");
 
 @inject(TermoApi)
 export class DialogoDeCriacaoDeTermo {
@@ -7,8 +8,22 @@ export class DialogoDeCriacaoDeTermo {
     this.termoApi = termoApi;
   }
 
+  @computedFrom('descricao')
+  get contadorCampoDescricao(){
+    return this.descricao ? this.descricao.length : 0;
+  }
+
+  @computedFrom('termo', 'descricao', 'link')
+  get habilitarBotao() {
+    return this.validarCamposFormulario();
+  }
+
   abrirDialogo() {
     document.getElementById("dialogoDeCriacaoDeTermo").style.display = 'block';
+    document.querySelectorAll('.alerta').forEach(alerta => {
+      alerta.textContent = '';
+      alerta.classList.remove('alerta-erro');
+    });
   }
 
   fecharDialogo() {
@@ -31,9 +46,34 @@ export class DialogoDeCriacaoDeTermo {
     );
   }
 
+  validarCamposFormulario() {
+    const termoValido = this.validarCampoTexto(this.termo, 'alerta-termo');
+    const descricaoValida = this.validarCampoTexto(this.descricao, 'alerta-descricao' );
+    const linkValido = this.validarCampoTexto(this.link, 'alerta-link');
+    return termoValido && descricaoValida && linkValido;
+  }
+
+  validarCampoTexto(campo, idAlerta) {
+    let valido = false;
+    valido = Boolean(campo) && campo.trim() != '';
+    const elemento = document.getElementById(idAlerta);
+    if (elemento) {
+      if (!valido) {
+        elemento.classList.add('alerta-erro');
+        elemento.textContent = 'Esse campo é obrigatório.';
+      }
+      else {
+        elemento.classList.remove('alerta-erro');
+        elemento.textContent = '';
+      }
+    }
+
+    return valido;
+  }
+
   limparVariaveis() {
-    this.termo = "";
-    this.descricao = "";
-    this.link = "";
+    this.termo = '';
+    this.descricao = '';
+    this.link = '';
   }
 }
