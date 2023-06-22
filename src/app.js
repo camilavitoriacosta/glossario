443a12f16efce326d253a979c325390a5ff226f2
Camila Vitoria <camilavitoria.c.costa@gmail.com>
2023-06-22 16:55:18 -0400

    ajustando visualização ordenacao de termos

--- src/app.js
+++ src/app.js
@@ -16,6 +16,10 @@ export class App {
   obterTermos() {
     this.termoApi.obterTermos().then(resposta => resposta.json()).then(resposta => {
       this.termos = resposta;
+      this.termos.forEach(elemento => {
+        elemento.termo =  _.capitalize(elemento.termo);
+      });
+      this.termos = _.orderBy(this.termos, ['termo'], ['asc']);
       this.termosExibicao = this.termos;
     });
   }
